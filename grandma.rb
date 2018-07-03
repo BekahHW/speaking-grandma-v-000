22be02fb4440ed7a8f36224a2b91026f59d2cb7b
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-03 10:38:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,10 @@ def speak_to_grandma(phrase)
  if "I LOVE YOU GRANDMA!"
    puts "I LOVE YOU TOO PUMPKIN!"
    
- elsif phrase.toup
-end
+ elsif phrase.upcase
+   puts "NO, NOT SINCE 1938!"
+   
+ else puts "HUH?! SPEAK UP, SONNY!"
+  end
+ end
 end
