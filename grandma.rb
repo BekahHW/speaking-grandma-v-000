fa6d0e21def7b64bf8a2d4154fd2ec223496dcac
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-03 10:39:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,8 @@ def speak_to_grandma(phrase)
  elsif phrase.upcase
    puts "NO, NOT SINCE 1938!"
    
- else "HUH?! SPEAK UP, SONNY!"
+ else  phrase
+   puts "HUH?! SPEAK UP, SONNY!"
    end
   end
  end
